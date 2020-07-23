11b8b931f06180b16525f3ec6645ed442a659e74
Emal Fiza <emal_fiza@hotmail.com>
2020-07-23 19:49:11 +0000

    Alter the fields name and description of the product model

--- products/models.py
+++ products/models.py
@@ -18,8 +18,8 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    name = models.CharField(max_length=100)
-    description = models.TextField(max_length=300)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
