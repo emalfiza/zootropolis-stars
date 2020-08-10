c672f83bb81cc73238a7c0a7c345e3f7e1eda7c9
Emal Fiza <emal_fiza@hotmail.com>
2020-08-10 11:44:24 +0000

    Create order models

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from products.models import Product
 
 # Create your models here.
+
+
+class Order(models.Model):
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = models.CharField(max_length=40, null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2,
+                                      null=False, default=0)
+
+    def __str__(self):
+        return f'{self.full_name}-{self.postcode}'
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+
+    def __str__(self):
+        return f'{self.quantity}-{self.product}'
