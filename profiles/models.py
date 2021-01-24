6b6cafdb3232a70db2ad42e11b1a5498013b9b72
Emal Fiza <emal_fiza@hotmail.com>
2021-01-24 11:40:24 +0000

    Add profile view and form

--- profiles/models.py
+++ profiles/models.py
@@ -8,20 +8,21 @@ from django.dispatch import receiver
 
 class UserProfile(models.Model):
     """
-    A user profile model for maintaining default 
+    A user profile model for maintaining default
     delivery information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=False)
-    default_country = models.CharField(max_length=40, null=True, blank=False)
-    default_postcode = models.CharField(max_length=20, null=True, blank=True)
-    default_town_or_city = models.CharField(max_length=40, null=True, blank=False)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=False)
     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True, blank=False)
     default_county = models.CharField(max_length=80, null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_country = models.CharField(max_length=40, null=True, blank=False)
 
     def __str__(self):
         return self.user.username
+
 
 @receiver(post_save, sender=User)
 def create_or_delete_user_profile(sender, instance, created, **kwargs):
